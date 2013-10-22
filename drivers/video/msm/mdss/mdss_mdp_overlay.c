8c3ceeef4ea131fe16b1489cb13e9a3862075d98
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-10-22 14:50:28 -0700

    msm: mdss: wait for ping pong during unblank kickoff
    During panel unblank a kickoff is done to turn on the panel so that the
    first update is not delayed. However since buffer sync and display
    commit is not called on this, need to make sure that the update finishes
    completely including the workqueue that signals the fences. Signaling
    the sync fence can potentially cause timeline to be ahead and end up
    signaling the acquire fences early leading to source tearing on command
    mode panels.
    
    Change-Id: I56edf8355e1c635c3da24f20405c3ae4fe31cf2e
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2400,6 +2400,10 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		rc = mdss_mdp_overlay_start(mfd);
 		if (!IS_ERR_VALUE(rc))
 			rc = mdss_mdp_overlay_kickoff(mfd, NULL);
+
+		if (mfd->panel_info->type == MIPI_CMD_PANEL)
+			mdss_mdp_display_wait4pingpong(mdp5_data->ctl);
+
 	} else {
 		rc = mdss_mdp_ctl_setup(mdp5_data->ctl);
 		if (rc)
