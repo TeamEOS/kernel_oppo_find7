e2343dd165d3fd9f9fab4ebb0ea3eeb071cb68a8
Praveen Chavan <pchavan@codeaurora.org>
2014-04-11 17:18:36 -0700

    msm: vidc: update with fixes and enhancements
    Add LTR support for video encoder.
    Add control to enable Hierarchical P num.
    Enable vp8 error resilience support.
    Avoid re-initialize of v4l2 spin lock.
    Fix a race condition with core instances list
    Add VP8 QP min and max control.
    Add support to set VP8 QP range.
    Update msm_media_info with MV Color format.
    Cleanup v4l2 buffer flags.
    Don't erase buffer flags in EBD
    Rewrite v4l2 cluster construction
    Detect mbaff information from firmware
    Add support for setting initial qp
    Configure and set hier-p layers
    Fix a race condition in cluster construction
    Indicate secure sessions in debugfs
    Remove non fatal error messages
    Query deblocker parameters from cluster
    Set error concealment color to FW
    Added v4l2 control to set the color of concealed MBs.
    Disable Hier-P when LTR is enabled
    
    Change-Id: I8e57be5284675c6371eb3a6046d91c575cdecb45
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_internal.h
+++ drivers/media/platform/msm/vidc/msm_vidc_internal.h
@@ -182,6 +182,7 @@ struct msm_vidc_core_capability {
 	u32 pixelprocess_capabilities;
 	struct hal_capability_supported scale_x;
 	struct hal_capability_supported scale_y;
+	struct hal_capability_supported hier_p;
 	u32 capability_set;
 	enum buffer_mode_type buffer_mode[MAX_PORT_NUM];
 };
@@ -220,7 +221,7 @@ struct msm_vidc_inst {
 	void *mem_client;
 	struct v4l2_ctrl_handler ctrl_handler;
 	struct completion completions[SESSION_MSG_END - SESSION_MSG_START + 1];
-	struct list_head ctrl_clusters;
+	struct v4l2_ctrl **cluster;
 	struct v4l2_fh event_handler;
 	struct msm_smem *extradata_handle;
 	wait_queue_head_t kernel_event_queue;
@@ -239,6 +240,7 @@ struct msm_vidc_inst {
 	struct list_head registered_bufs;
 	bool map_output_buffer;
 	atomic_t get_seq_hdr_cnt;
+	struct v4l2_ctrl **ctrls;
 };
 
 extern struct msm_vidc_drv *vidc_driver;
@@ -258,8 +260,6 @@ struct msm_vidc_ctrl {
 	u32 step;
 	u32 menu_skip_mask;
 	const char * const *qmenu;
-	u32 cluster;
-	struct v4l2_ctrl *priv;
 };
 
 void handle_cmd_response(enum command_response cmd, void *data);
