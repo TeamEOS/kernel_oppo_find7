papi92/android_kernel_oppo_find7

1.Adjusted backlight duration when the screen is turned on. 2.Optimized Waves effect. 3.Improved charging performance. 4.Optimized LCD display effect.

drivers/leds/leds-qpnp.c
@@ -1224,11 +1224,24 @@ error_flash_set:
 	return rc;
 }
 
+/*OPPO yuyi 2014-03-22 add begin for delay button_backlight*/
+#ifdef VENDOR_EDIT
+extern int button_backlight;
+#endif
+/*OPPO yuyi 2014-03-22 add end for delay button_backlight*/
+
 static int qpnp_kpdbl_set(struct qpnp_led_data *led)
 {
 	int duty_us;
 	int rc;
-
+/*OPPO yuyi 2014-03-22 add begin for delay button_backlight*/
+#ifdef VENDOR_EDIT
+	if(button_backlight == 1 ) {
+		msleep(600);
+		button_backlight ++;
+	}
+#endif
+/*OPPO yuyi 2014-03-22 add end for delay button_backlight*/	
 	if (led->cdev.brightness) {
 		if (!led->kpdbl_cfg->pwm_cfg->blinking)
 			led->kpdbl_cfg->pwm_cfg->mode =

TeamEOS/kernel_oppo_find7

Conflicts:
	arch/arm/mach-msm/include/mach/qdsp6v2/rtac.h
	drivers/power/qpnp-charger.c
	include/sound/q6adm-v2.h
	include/sound/q6asm-v2.h
	include/sound/q6audio-v2.h
	sound/soc/msm/qdsp6v2/msm-pcm-q6-v2.c
	sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
	sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.h
	sound/soc/msm/qdsp6v2/q6adm.c
	sound/soc/msm/qdsp6v2/q6asm.c
	sound/soc/msm/qdsp6v2/rtac.c

drivers/leds/leds-qpnp.c
@@ -1224,11 +1224,24 @@ error_flash_set:
 	return rc;
 }
 
+/*OPPO yuyi 2014-03-22 add begin for delay button_backlight*/
+#ifdef CONFIG_MACH_OPPO
+extern int button_backlight;
+#endif
+/*OPPO yuyi 2014-03-22 add end for delay button_backlight*/
+
 static int qpnp_kpdbl_set(struct qpnp_led_data *led)
 {
 	int duty_us;
 	int rc;
-
+/*OPPO yuyi 2014-03-22 add begin for delay button_backlight*/
+#ifdef CONFIG_MACH_OPPO
+	if(button_backlight == 1 ) {
+		msleep(600);
+		button_backlight ++;
+	}
+#endif
+/*OPPO yuyi 2014-03-22 add end for delay button_backlight*/	
 	if (led->cdev.brightness) {
 		if (!led->kpdbl_cfg->pwm_cfg->blinking)
 			led->kpdbl_cfg->pwm_cfg->mode =
@@ -3576,4 +3589,3 @@ module_exit(qpnp_led_exit);
 MODULE_DESCRIPTION("QPNP LEDs driver");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("leds:leds-qpnp");
-